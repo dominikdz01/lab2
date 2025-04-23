a8bba9460a689a6c11f72d6f042bc3b76cbefefc
dominikdz01 <dominik.dziegielewski01@gmail.com>
2025-04-23 14:14:58 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+x = int(input("Podaj liczbę x: "))
+y = int(input("Podaj liczbę y: "))
+
+if x > y:
+    print("x jest większe od y")
+elif x < y:
+    print("x jest mniejsze od y")
+else:
+    print("x i y są równe")
---
+++ program3.py
@@ -0,0 +1,3 @@
+oceny = {'Jan': 5, 'Anna': 4, 'Tomek': 3}
+for imie, ocena in oceny.items():
+    print(f"{imie} otrzymał(a) ocenę {ocena}")
